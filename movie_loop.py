e9ccbf56aa982e209e82ccd588eb9bb15a514231
Dan Mateer <dan@pullrequest.com>
2018-01-09 09:25:41 -0600

    adding movie_loop file

---
+++ movie_loop.py
@@ -0,0 +1,10 @@
+movies = ["ID4", "Dumb & Dumber", "Tremors", "The Arrival", "Face/Off", "Species"]
+
+for film in movies:
+  if film == "Tremors":
+    print film, "... so good"
+  elif film == "The Arrival":
+    print film, "(Charlie Sheen was outstanding)"
+  print "A great 90's movie:", film
+else:
+  print "I miss the 90's"
